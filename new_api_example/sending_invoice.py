a655667d478053449988dc2a93e86d5db5d79f75
Bale admin <35300274+baleadmin@users.noreply.github.com>
2019-03-13 10:14:49 +0330

    add getting payment of invoice message example

--- new_api_example/sending_invoice.py
+++ new_api_example/sending_invoice.py
@@ -1,13 +1,15 @@
+import json
 import logging
 
 from telegram import Bot, LabeledPrice
-from telegram.ext import Updater, CommandHandler, RegexHandler, ConversationHandler
+from telegram.ext import Updater, CommandHandler, RegexHandler, ConversationHandler, MessageHandler, Filters
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.DEBUG)
+logger = logging.getLogger()
 
-INVOICE = 0
+INVOICE, PAYMENT = range(2)
 
 
 def start(bot: Bot, update):
@@ -19,6 +21,15 @@ def invoice(bot, update):
     bot.send_invoice(chat_id=update.message.chat_id, title="title", description="description", payload="payload",
                      provider_token="6037603760376037", start_parameter="", currency="IRR",
                      prices=[LabeledPrice('label1', int(update.message.text))])
+    return PAYMENT
+
+
+def payment(bot, update):
+    successful_payment = update.message.successful_payment
+    logger.info("SuccessfulPayment with payload: %s", successful_payment.invoice_payload)
+    invoice_payload = json.loads(successful_payment.invoice_payload)
+    update.message.reply_text(
+        "successfulPayment with traceNo: {}".format(invoice_payload.get('traceNo')))
     return ConversationHandler.END
 
 
@@ -30,6 +41,7 @@ conversation_handler = ConversationHandler(
 
     states={
         INVOICE: [RegexHandler(pattern='^\d+$', callback=invoice)],
+        PAYMENT: [MessageHandler(filters=Filters.successful_payment, callback=payment)],
     },
 
     fallbacks=[]
@@ -37,7 +49,6 @@ conversation_handler = ConversationHandler(
 
 updater.dispatcher.add_handler(conversation_handler)
 
-updater.bot.delete_webhook()
 updater.start_polling(poll_interval=2)
 # you can replace above line with commented below lines to use webhook instead of polling
 # updater.start_webhook(listen=os.getenv('WEB_HOOK_IP', ""), port=int(os.getenv('WEB_HOOK_PORT', "")),
